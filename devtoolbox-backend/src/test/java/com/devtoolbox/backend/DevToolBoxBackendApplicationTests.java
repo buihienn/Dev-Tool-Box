836e62b4ea261cb9975ec24d7189cdfd5f8c9a01
BuiHien <buihien147456369@gmail.com>
2025-03-23 20:16:06 +0700

    Initial commit

---
+++ devtoolbox-backend/src/test/java/com/devtoolbox/backend/DevToolBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtoolbox.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevToolBoxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
