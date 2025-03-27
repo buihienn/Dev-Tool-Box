4cc88742276aaf99a96278f4a9dba54b899df98d
DinhHoHo <hpv25072004@gmail.com>
2025-03-28 02:47:38 +0700

    [FIX] Fix app to show header, sidebar after merge.

--- devtoolbox-frontend/src/App.js
+++ devtoolbox-frontend/src/App.js
@@ -1,28 +1,36 @@
 import './App.css';
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SidebarProvider } from './context/SidebarContext';
 import ToolLayout from './pages/ToolLayout';
 import UserDashboard from './pages/UserDashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import TokenGeneratorTool from './pages/TokenGeneratorTool';
+import HashText from './pages/HashText';
+import Bcrypt from './pages/Bcrypt';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ToolLayout />}/>
+    <SidebarProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Đặt ToolLayout làm layout chính */}
+          <Route path="/" element={<ToolLayout />}>
+            <Route index element={<UserDashboard />} />
+            <Route path="token-generator" element={<TokenGeneratorTool />} />
+            <Route path="hash-text" element={<HashText />} />
+            <Route path="bcrypt" element={<Bcrypt />} />
+          </Route>
 
-        <Route path="/login" element={<Login />} />       
-        <Route path="/register" element={<Register />} />
-        <Route index element={<UserDashboard />} />
-            
-        {/* Các công cụ */}
-        <Route path="/token-generator" element={<TokenGeneratorTool />} />
-        <Route path="/hash-text" element={<HashText />} />
-        <Route path="/bcrypt" element={<Bcrypt />} />
-        <Route path="*" element={<div>Không tìm thấy trang!</div>} />
-      </Routes>
-    </Router>
+          {/* Các route không cần ToolLayout */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<div>Không tìm thấy trang!</div>} />
+        </Routes>
+      </BrowserRouter>
+    </SidebarProvider>
   );
 }
 
